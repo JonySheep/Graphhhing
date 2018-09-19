32e493cef48ab1da344a638574acaf4514e51c59
Yang <yyqyang222@163.com>
2018-09-19 10:51:48 +0800

    add：Element and mainpgae background

---
+++ Web/src/pages/GraphPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="back-ground">
+    <el-card class="canvas-container">
+
+    </el-card>
+  </div>
+</template>
+
+<script>
+import '../styles/background.css'
+
+export default {
+  name: 'GraphPage',
+  data () {
+    return {
+      msg: "Let's Drawing!"
+    }
+  }
+}
+</script>
+
+<style>
+  .canvas-container{
+    height:600px;
+    width: 100%;
+    background-color: white;
+  }
+</style>
--- Web/src/router/index.js
+++ Web/src/router/index.js
@@ -1,15 +1,18 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from 'vue-router';
+import GraphPage from '../pages/GraphPage.vue';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'GraphPage',
+      component: GraphPage,
+      meta: {
+        title: "Graphhhing"
+      }
+    },
   ]
 })
